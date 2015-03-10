138ab160ceaca3032017328db92f97fb17e525bb
Paulo Caldeira <pmcjr@br.ibm.com>
2015-03-10 15:29:27 -0300

    starter application commited

---
+++ README.txt
@@ -0,0 +1,26 @@
+# Node.js Starter Application
+
+Bluemix provides a Node.js starter application as a template so that you can add your code and push the changes back to the Bluemix environment.
+
+
+## Files
+
+The Node.js starter application has files as below:
+
+*   app.js
+
+	This file contains the server side JavaScript code for your application written using the Node.js API
+
+*   views/
+
+	This directory contains the views of the application. It is required by the express framework and jade template engine in this sample application.
+
+*   public/
+
+	This directory contains public resources of the application. It is required by the express framework in this sample application.
+
+*   package.json
+
+	This file is required by the Node.js environment. It specifies this Node.js project name, dependencies, and other configurations of your Node.js application.
+
+
---
+++ app.js
@@ -0,0 +1,41 @@
+/*jshint node:true*/
+
+// app.js
+// This file contains the server side JavaScript code for your application.
+// This sample application uses express as web application framework (http://expressjs.com/),
+// and jade as template engine (http://jade-lang.com/).
+
+var express = require('express');
+
+// setup middleware
+var app = express();
+app.use(app.router);
+app.use(express.errorHandler());
+app.use(express.static(__dirname + '/public')); //setup static public directory
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
+
+// render index page
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+// There are many useful environment variables available in process.env.
+// VCAP_APPLICATION contains useful information about a deployed application.
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+// TODO: Get application information and use it in your app.
+
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+// TODO: Get service credentials and communicate with bluemix services.
+
+// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+// The port on the DEA for communication with the application:
+var port = (process.env.VCAP_APP_PORT || 3000);
+// Start server
+app.listen(port, host);
+console.log('App started on port ' + port);
+

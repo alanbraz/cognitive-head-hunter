678fd4e65e080ffff86b51cc0255f5b115b93095
Paulo Caldeira <pmcjr@br.ibm.com>
2015-03-10 16:01:43 -0300

    concept insights demo files added;

--- app.js
+++ app.js
@@ -1,41 +1,76 @@
-/*jshint node:true*/
+/**
+ * Copyright 2014 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// app.js
-// This file contains the server side JavaScript code for your application.
-// This sample application uses express as web application framework (http://expressjs.com/),
-// and jade as template engine (http://jade-lang.com/).
+'use strict';
 
-var express = require('express');
+var express = require('express'),
+  app = express(),
+  bluemix = require('./config/bluemix'),
+  watson = require('watson-developer-cloud'),
+  extend = require('util')._extend;
 
-// setup middleware
-var app = express();
-app.use(app.router);
-app.use(express.errorHandler());
-app.use(express.static(__dirname + '/public')); //setup static public directory
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
+// Bootstrap application settings
+require('./config/express')(app);
 
-// render index page
+// if bluemix credentials exists, then override local
+var credentials = extend({
+  version: 'v1',
+  username: '<username>',
+  password: '<password>'
+}, bluemix.getServiceCreds('concept_insights')); // VCAP_SERVICES
+
+// Create the service wrapper
+var conceptInsights = watson.concept_insights(credentials);
+
 app.get('/', function(req, res){
-	res.render('index');
+    res.render('index');
 });
 
-// There are many useful environment variables available in process.env.
-// VCAP_APPLICATION contains useful information about a deployed application.
-var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
-// TODO: Get application information and use it in your app.
+app.get('/label_search', function (req, res) {
+  var payload = extend({
+    func:'labelSearch',
+    limit: 4,
+    prefix:true,
+    concepts:true,
+  }, req.query);
 
-// VCAP_SERVICES contains all the credentials of services bound to
-// this application. For details of its content, please refer to
-// the document or sample of each service.
-var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
-// TODO: Get service credentials and communicate with bluemix services.
+  conceptInsights.labelSearch(payload, function(error, result) {
+    if (error)
+      return res.status(error.error ? error.error.code || 500 : 500).json(error);
+    else
+      return res.json(result);
+  });
+});
 
-// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-// The port on the DEA for communication with the application:
-var port = (process.env.VCAP_APP_PORT || 3000);
-// Start server
-app.listen(port, host);
-console.log('App started on port ' + port);
+app.get('/semantic_search', function (req, res) {
+  var payload = extend({
+    func:'semanticSearch',
+  }, req.query);
 
+  // ids needs to be stringify
+  payload.ids = JSON.stringify(payload.ids);
+
+  conceptInsights.semanticSearch(payload, function(error, result) {
+    if (error)
+      return res.status(error.error ? error.error.code || 500 : 500).json(error);
+    else
+      return res.json(result);
+  });
+});
+
+var port = process.env.VCAP_APP_PORT || 3000;
+app.listen(port);
+console.log('listening at:', port);
